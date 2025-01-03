261d2f0efa1696924a48d9e174a26fb0cf439359
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2025-01-02 20:37:38 -0500

    v3.3.0

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: GitHub Profile Widget
  * Description: This is a plugin that shows your GitHub profile with a simple widget.
- * Version: 3.2.3
+ * Version: 3.3.0
  * Author: Henrique Dias and Luís Soares (Refactors) and DJABHipHop
  * Author URI: https://github.com/WP-Developer-Hub
  * Requires PHP: 7.2
